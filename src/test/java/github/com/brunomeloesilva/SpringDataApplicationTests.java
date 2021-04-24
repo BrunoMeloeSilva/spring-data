987418cc0b4605d7163a592e0db97a6ba9a9860f
brunomeloesilva <brunomeloesilva@gmail.com>
2021-04-24 12:11:08 -0400

    Abertura do projeto

---
+++ src/test/java/github/com/brunomeloesilva/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.brunomeloesilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
